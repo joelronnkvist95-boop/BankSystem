f684032a0f08e6d18e87067ae9ec8ed6e0ba2f48
Joel Rönnkvist <joelronnkvist95@gmail.com>
2026-09-24 10:02:31 +0200

    Add project files.

---
+++ BankSystem/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace BankSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till Project Bank");
+
+            while (true)
+            {
+                bool loggedIn = false;
+                
+                Console.Clear();
+                Console.Write("Skriv ditt användarnamn: ");
+                string userInput = Console.ReadLine();
+
+                int incorrectPasswordCount = 0;
+
+                
+
+
+
+                if(userInput == userName)
+                {
+                    Console.WriteLine("Rätt användarnamn");
+                    Console.Write("Knappa in ditt lösenord:");
+                    string passwordInput = Console.ReadLine();
+
+                    bool correctUserName = true;
+
+                    while (correctUserName)
+                    {
+                        if (passwordInput == password)
+                        {
+                            bool loggedIn = true;
+                            Console.WriteLine("Rätt lösenord och användarnamn. Du är nu inloggad");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel lösenord, försök igen.");
+                        }
+                    }
+
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Felaktigt användarnamn. Försök igen");
+                }
+            }
+            
+            
+        }
+    }
+    class User
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        public
+    }
+    class Account
+    {
+        public string accountNumber; { get; set; }
+        public decimal accountBalance; { get; set; }
+        public string accountType; { get; set; }
+
+    }
+}
+
